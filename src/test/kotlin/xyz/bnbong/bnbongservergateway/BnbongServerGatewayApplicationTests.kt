a2fe6acc789a2c846e9bfdb974ce67de96cdac34
bnbong <bbbong9@gmail.com>
2023-12-19 17:36:41 +0900

    initial project settings - JDK & JAVA 17 config

---
+++ src/test/kotlin/xyz/bnbong/bnbongservergateway/BnbongServerGatewayApplicationTests.kt
@@ -0,0 +1,13 @@
+package xyz.bnbong.bnbongservergateway
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BnbongServerGatewayApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
